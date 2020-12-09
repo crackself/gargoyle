eabd8ccf9039e87e82899ab1a6a9ce430440f696
crackself <crackself@gmail.com>
2020-12-09 15:38:46 +0800

    update translte for sta_manager in ZH-CN

---
+++ package/plugin-gargoyle-i18n-SimplifiedChinese-ZH-CN/files/www/i18n/SimplifiedChinese-ZH-CN/sta_manager.js
@@ -0,0 +1,39 @@
+﻿/*
+ * UTF-8 (with BOM) English-EN text strings for sta_manager.sh html elements
+ */
+
+stamgrStr.mStaMgr="STA 管理器";
+stamgrStr.StaMgrSettings="STA 管理器设置";
+stamgrStr.StaMgrEn="启用STA管理器";
+stamgrStr.MaxConnAttempts="最大连接尝试数";
+stamgrStr.MaxWait="尝试连接间的最大等待时间";
+stamgrStr.DisconnectQualThresh="断开连接的质量阈值";
+stamgrStr.ConnectQualThresh="连接的质量阈值";
+stamgrStr.BlacklistTimer="将连接失败黑名单";
+stamgrStr.BlacklistTime="剩余时间";
+stamgrStr.Status="运行状态";
+stamgrStr.WirelessAPs="无线接入点";
+stamgrStr.AddAP="添加AP";
+stamgrStr.EAP="编辑AP";
+stamgrStr.LastUpdate="最近更新";
+stamgrStr.CurrAP="当前AP";
+stamgrStr.Connected="已连接";
+stamgrStr.BlacklistAP="拉黑AP";
+stamgrStr.NoStacfgErr="必须将无线设置为客户端或客户端+ AP模式才能使用STA管理器";
+stamgrStr.ScanWifi="扫描WiFi";
+
+//Javascript
+stamgrStr.cfgid="设置ID";
+stamgrStr.Band="带宽";
+stamgrStr.Encryption="加密";
+stamgrStr.Password="密码";
+stamgrStr.AErr="无法添加行.";
+stamgrStr.upErr="无法更新行.";
+stamgrStr.discontoohighErr="断开质量必须低于90％";
+stamgrStr.contoolowErr="连接质量必须高于30％";
+stamgrStr.discongtconErr="断开质量必须低于连接质量";
+stamgrStr.CurWanCfg="当前的WAN网络";
+stamgrStr.Other="其它";
+stamgrStr.Scanning="扫描中... 请稍后.";
+stamgrStr.NoWifi="找不到网络！";
+
